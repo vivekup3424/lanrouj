fc7f226be33063d3d72f62a1d9777f6d9979f9a5
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-07-22 16:54:16 +0530

    update

---
+++ practice.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Define color variables
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+RESET='\033[0m'
+
+# Print colored text
+echo -e "${RED}This is red text${RESET}"
+echo -e "${GREEN}This is green text${RESET}"
+echo -e "${YELLOW}This is yellow text${RESET}"
+echo -e "${BLUE}This is blue text${RESET}"
+echo -e "${MAGENTA}This is magenta text${RESET}"
+echo -e "${CYAN}This is cyan text${RESET}"
+echo -e "${WHITE}This is white text${RESET}"
